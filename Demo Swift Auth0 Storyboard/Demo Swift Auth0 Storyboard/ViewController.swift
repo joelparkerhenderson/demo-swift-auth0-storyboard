76ee18471ef69c21f2fa4b00e28f739db0acc65e
Joel Parker Henderson <joel@joelparkerhenderson.com>
2020-08-17 18:17:32 -0700

    Init

---
+++ Demo Swift Auth0 Storyboard/Demo Swift Auth0 Storyboard/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Demo Swift Auth0 Storyboard
+//
+//  Created by Joel on 2020-08-17.
+//  Copyright © 2020 JoelParkerHenderson.com. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Demo Swift Auth0 Storyboard/Demo Swift Auth0 StoryboardTests/Demo_Swift_Auth0_StoryboardTests.swift
@@ -0,0 +1,34 @@
+//
+//  Demo_Swift_Auth0_StoryboardTests.swift
+//  Demo Swift Auth0 StoryboardTests
+//
+//  Created by Joel on 2020-08-17.
+//  Copyright © 2020 JoelParkerHenderson.com. All rights reserved.
+//
+
+import XCTest
+@testable import Demo_Swift_Auth0_Storyboard
+
+class Demo_Swift_Auth0_StoryboardTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
