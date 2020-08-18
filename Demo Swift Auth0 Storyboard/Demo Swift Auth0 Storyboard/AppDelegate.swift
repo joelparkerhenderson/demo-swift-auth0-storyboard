039b62e5bd2edeac4f72922c35c40649745f3584
Joel Parker Henderson <joel@joelparkerhenderson.com>
2020-08-17 21:12:54 -0700

    Add Auth0 to AppDelegate

--- Demo Swift Auth0 Storyboard/Demo Swift Auth0 Storyboard/AppDelegate.swift
+++ Demo Swift Auth0 Storyboard/Demo Swift Auth0 Storyboard/AppDelegate.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import CoreData
+import Auth0
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -31,6 +32,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Called when the user discards a scene session.
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
+    }
+
+    // MARK: Auth0 authentication
+
+    func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any]) -> Bool {
+        return Auth0.resumeAuth(url, options: options)
     }
 
     // MARK: - Core Data stack
