649c4c1e21c492db9861f4f85728f190a796f18d
Joel Parker Henderson <joel@joelparkerhenderson.com>
2020-08-17 21:23:18 -0700

    Add Auth0 to ViewController

--- Demo Swift Auth0 Storyboard/Demo Swift Auth0 Storyboard/ViewController.swift
+++ Demo Swift Auth0 Storyboard/Demo Swift Auth0 Storyboard/ViewController.swift
@@ -7,14 +7,28 @@
 //
 
 import UIKit
+import Auth0
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        Auth0
+            .webAuth()
+            .scope("openid profile")
+            .audience("https://joelparkerhenderson.auth0.com/userinfo")
+            .start {
+                switch $0 {
+                case .failure(let error):
+                    // Handle the error
+                    print("Error: \(error)")
+                case .success(let credentials):
+                    // Do something with credentials e.g.: save them.
+                    // Auth0 will automatically dismiss the login page
+                    print("Credentials: \(credentials)")
+                }
+        }
     }
-
-
 }
 
